da4af697d9edc96c9d18d983a47279c06e0efb2e
Joao Gabriel <jgabriellima10@gmail.com>
2022-10-09 21:09:28 -0300

    criando o metodo listar para o Cliente

--- src/main/java/com/challenge/carsales/service/ClienteService.java
+++ src/main/java/com/challenge/carsales/service/ClienteService.java
@@ -2,9 +2,12 @@ package com.challenge.carsales.service;
 
 import com.challenge.carsales.entitie.Cliente;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ClienteService {
+
+    List<Cliente> listCliente();
 
     Optional<Cliente> findById(Long id);
 
--- src/main/java/com/challenge/carsales/service/ClienteServiceImpl.java
+++ src/main/java/com/challenge/carsales/service/ClienteServiceImpl.java
@@ -5,6 +5,7 @@ import com.challenge.carsales.repositorie.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -12,6 +13,11 @@ public class ClienteServiceImpl implements ClienteService{
 
     @Autowired
      private ClienteRepository clienteRepository;
+
+    @Override
+    public List<Cliente> listCliente(){
+        return clienteRepository.findAll();
+    }
 
     @Override
      public Optional<Cliente> findById(Long id){
@@ -25,9 +31,12 @@ public class ClienteServiceImpl implements ClienteService{
         return clienteDelete;
      }
 
+     @Override
      public Cliente createCliente(Cliente cliente){
         return clienteRepository.save(cliente);
      }
+
+
 
 
 
